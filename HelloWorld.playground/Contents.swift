79d3cc09bf5110b5e11bbae86ad9b0bf26548c24
runclimbhike <runclimbhike@users.noreply.github.com>
2018-12-21 22:35:29 -0600

    Add files via upload

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,31 @@
+print("Hello World")
+// prints hello world
+var age = 5
+print(age)
+
+print("This is better than python")
+
+// create arrays and dictionaries using brackets
+var shoppingList = ["salmon", "water", "milk"]
+
+shoppingList[1]
+shoppingList[0]
+
+// dictionary
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+]
+occupations["Jayne"] = "Public Relations"
+
+occupations["Kevin"] = "IT Professional"
+// print occupations will display the dictionary
+print(occupations)
+
+// append to an array
+shoppingList.append("beer")
+
+// use a code snippet to show how to use a for loop
+for item in shoppingList {
+    print(item.enumerated())
+}
